64983924b131241b02b2b2782a57a843c7cc8445
igor-shatrov <56680830+igor-shatrov@users.noreply.github.com>
2020-05-28 12:25:26 +0300

    Изменены иконки. Добавлен фильтр в инпут телефона

--- script.js
+++ script.js
@@ -1,75 +1,90 @@
-//Плавный переход между якорями
-function handleButtonClick(className) {
-    document.querySelector("." + className).scrollIntoView({ block: "start", behavior: "smooth" });
-}
+(function() {
+    //Плавный переход между якорями
+    function handleButtonClick(className) {
+        document.querySelector("." + className).scrollIntoView({ block: "start", behavior: "smooth" });
+    }
 
-document.querySelectorAll('.blk-1-href').forEach(elem => { elem.addEventListener('click', () => { handleButtonClick('block-1') }) });
-document.querySelectorAll('.blk-3-href').forEach(elem => { elem.addEventListener('click', () => { handleButtonClick('block-3') }) });
-document.querySelectorAll('.blk-5-href').forEach(elem => { elem.addEventListener('click', () => { handleButtonClick('block-5') }) });
-document.querySelectorAll('.footer-href').forEach(elem => { elem.addEventListener('click', () => { handleButtonClick('address') }) });
+    document.querySelectorAll('.blk-1-href').forEach(elem => { elem.addEventListener('click', () => { handleButtonClick('block-1') }) });
+    document.querySelectorAll('.blk-3-href').forEach(elem => { elem.addEventListener('click', () => { handleButtonClick('block-3') }) });
+    document.querySelectorAll('.blk-5-href').forEach(elem => { elem.addEventListener('click', () => { handleButtonClick('block-5') }) });
+    document.querySelectorAll('.footer-href').forEach(elem => { elem.addEventListener('click', () => { handleButtonClick('address') }) });
 
-//Инициализация select Materialize
-document.addEventListener("DOMContentLoaded", function() {
-    var elems = document.querySelectorAll("select");
-    var instances = M.FormSelect.init(elems, {});
-});
+    //Инициализация select Materialize
+    document.addEventListener("DOMContentLoaded", function() {
+        var elems = document.querySelectorAll("select");
+        var instances = M.FormSelect.init(elems, {});
+    });
 
 
-//При выборе автомобиля появляется нужная карточка
-document.querySelector('#select-auto').onchange = function() {
-    document.querySelectorAll('.card-auto').forEach(elem => {
-        elem.classList.add('hide');
+    //При выборе автомобиля появляется нужная карточка
+    document.querySelector('#select-auto').onchange = function() {
+        document.querySelectorAll('.card-auto').forEach(elem => {
+            elem.classList.add('hide');
+        })
+        document.getElementById(`card-${this.value}`).classList.remove('hide');
+    }
+
+    //Адаптивное меню
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, {});
+    });
+
+
+    //Отправка данных из формы
+    document.querySelector("#submit-btn").onclick = () => {
+        let name = document.querySelector("#name").value;
+        let phone = document.querySelector("#phone").value;
+        let text = document.querySelector("#text").value;
+        document.querySelector("#name").value = '';
+        document.querySelector("#phone").value = '';
+        document.querySelector("#text").value = '';
+        document.querySelector('#submit-btn').classList.add('disabled');
+        sendRequest('POST', 'send.php', { name: name, phone: phone, text: text })
+            .then(response => {
+                if (response === 'error') {
+                    M.toast({ html: 'Что-то пошло не так :( Пожалуйста, позвоните нам по телефону' })
+                } else if (response == 'success') {
+                    M.toast({ html: 'Спасибо за заявку! Наш менеджер перезвонит вам в ближайшее время' });
+                }
+            })
+    }
+
+    //Проверка вводимых данных на 
+    document.querySelector('#phone').addEventListener('keydown', checkValidNumbers);
+
+    function checkValidNumbers(e) {
+        if (e.keyCode >= 48 && e.keyCode <= 57) {} else if (e.keyCode == 107) {} else if (e.keyCode == 8) {} else {
+            e.preventDefault();
+        }
+        if (e.target.value.length >= 13 && e.keyCode != 8) {
+            e.preventDefault();
+        }
+
+    }
+
+    //Проверка формы на валидность
+    document.querySelectorAll('.input-field input').forEach(elem => {
+        elem.addEventListener('input', checkFormData);
     })
-    document.getElementById(`card-${this.value}`).classList.remove('hide');
-}
 
-//Адаптивное меню
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
-});
+    function checkFormData() {
+        if (document.querySelector("#name").value != "" && document.querySelector("#phone").value.length >= 13) {
+            document.querySelector('#submit-btn').classList.remove('disabled');
+        }
+    }
 
-
-//Отправка данных из формы
-document.querySelector("#submit-btn").onclick = () => {
-    let name = document.querySelector("#name").value;
-    let phone = document.querySelector("#phone").value;
-    let text = document.querySelector("#text").value;
-    document.querySelector("#name").value = '';
-    document.querySelector("#phone").value = '';
-    document.querySelector("#text").value = '';
-    document.querySelector('#submit-btn').classList.add('disabled');
-    sendRequest('POST', 'send.php', { name: name, phone: phone, text: text })
-        .then(response => {
-            if (response === 'error') {
-                M.toast({ html: 'Что-то пошло не так :( Пожалуйста, позвоните нам по телефону' })
-            } else if (response == 'success') {
-                M.toast({ html: 'Спасибо за заявку! Наш менеджер перезвонит вам в ближайшее время' });
+    function sendRequest(method, url, body = null) {
+        return fetch(url, {
+            method: method,
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' }
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return 'error';
             }
         })
-}
-
-//Проверка формы на валидность
-document.querySelectorAll('.input-field input').forEach(elem => {
-    elem.addEventListener('input', checkFormData);
-})
-
-function checkFormData() {
-    if (document.querySelector("#name").value != "" && document.querySelector("#phone").value.length >= 13) {
-        document.querySelector('#submit-btn').classList.remove('disabled');
     }
-}
-
-function sendRequest(method, url, body = null) {
-    return fetch(url, {
-        method: method,
-        body: JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' }
-    }).then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return 'error';
-        }
-    })
-}
+}());
